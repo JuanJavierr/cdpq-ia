d39e08a9691d5b99d721f484e938f8eb181b2611
JuanJavierr <xavierpeladeau12@hotmail.com>
2024-12-30 16:41:52 -0500

    Fix relative import

--- utils.py
+++ utils.py
@@ -19,19 +19,66 @@ from plotly.subplots import make_subplots
 from pathlib import Path
 
 
+#### Deep Learning utils
+def evaluate_by_horizon(forecasts_df):
+    forecasts_df["abs_pct_error"] = ((forecasts_df["US_TB_YIELD_10YRS"] - forecasts_df["forecast"]) / forecasts_df["US_TB_YIELD_10YRS"]).abs()
+    forecasts_df["squared_error"] = forecasts_df["error"]**2
+    grouped = forecasts_df.groupby(by="horizon").mean()[["abs_pct_error", "squared_error"]].add_prefix("mean_")
+    grouped["root_mean_squared_error"] = grouped["mean_squared_error"]**0.5
 
-config = {
-    "start_date_train": "1970-01-01",
-    "end_date_train": "2015-12-31",
-    "start_date_test": "2016-01-01",
-    "end_date_test": "2022-12-31",
-    "min_variable_count": 2,
-    "max_variable_count": 20,
-}
+    return grouped
 
+
+def save_results(hparams, eval_metrics, output_path):
+    output_path = Path(output_path)
+    include_header = not output_path.exists()
+    results = pd.concat([pd.Series(hparams), eval_metrics.mean()])
+    pd.DataFrame(results).T.to_csv(output_path, mode="a", header=include_header, index=False)
+
+
+
+#### Plot utils
+def plot_training_history(train_losses, val_losses):
+    # Create the figure
+    fig = go.Figure()
+
+    # Add traces for training and validation loss
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, len(train_losses) + 1)),
+            y=train_losses,
+            name='Training Loss',
+            line=dict(color='blue')
+        )
+    )
+    
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, len(val_losses) + 1)),
+            y=val_losses,
+            name='Validation Loss',
+            line=dict(color='red')
+        )
+    )
+
+    # Update layout
+    fig.update_layout(
+        title='Training and Validation Loss Over Time',
+        xaxis_title='Epoch',
+        yaxis_title='Loss',
+        hovermode='x unified',
+        template='plotly_white'
+    )
+
+    # Show the plot
+    fig.show()
+
+
+
+### Other utils
 def load_data_dict():
     tables = []
-    with open("data/data_concours_feature_descriptions.txt") as dd:
+    with open(Path(__file__).parent / "data/data_concours_feature_descriptions.txt") as dd:
         dd_json = json.load(dd)
         for k in dd_json.keys():
             tables.append(pd.json_normalize(dd_json[k]))
@@ -39,17 +86,23 @@ def load_data_dict():
     data_dictionary = pd.concat(tables).reset_index(drop=True)
     return data_dictionary
 
+def get_feature_desc(feature: str):
+    data_dictionary = load_data_dict()
+    
+    feature_clean = feature.replace("_YOY", "").replace("_QOQ", "").replace("_MOM", "").replace("_WOW", "")
+    feature_clean = re.sub(r"_\d{1,2}(?:YRS|MTHS|YR)", "", feature_clean)
+    desc = data_dictionary.loc[data_dictionary["FEATURE_NAME"] == feature_clean]
+    if desc.empty:
+        raise Exception(f"Feature {feature} not found in data dictionary")
+    return desc.iloc[0]["FEATURE_DESCRIPTION"]
 
-data_dictionary = load_data_dict()
+def print_features_not_in_data_dict(df, data_dict):
+    for feature in df.columns:
+        try:
+            get_feature_desc(feature)
+        except:
+            print(f"Feature {feature} not found in data dictionary")
 
-def train_test_split(df, label_col = "FFED_diff"):
-    y_train = df.loc[config["start_date_train"]:config["end_date_train"], label_col]
-    X_train = df.loc[config["start_date_train"]:config["end_date_train"]].drop(columns=[label_col, "FFED"])
-
-    y_test = df.loc[config["start_date_test"]:config["end_date_test"], label_col]
-    X_test = df.loc[config["start_date_test"]:config["end_date_test"]].drop(columns=[label_col, "FFED"])
-
-    return X_train, y_train, X_test, y_test
 
 def lag_monthly_macro_variables(df):
     """
@@ -70,16 +123,15 @@ def lag_monthly_macro_variables(df):
     return df
 
 
-
 def load_data():
 
     ### Load SF FED data
-    sf_df = pd.read_excel("data/sf_fed/news_sentiment_data.xlsx", sheet_name="Data")
+    sf_df = pd.read_excel(Path(__file__).parent / "data/sf_fed/news_sentiment_data.xlsx", sheet_name="Data")
     sf_df = sf_df.set_index("date").asfreq("B").resample("ME").mean()
 
 
     ###
-    df = pd.read_csv("./data/data_concours.csv", index_col=0)
+    df = pd.read_csv(Path(__file__).parent / "data/data_concours.csv", index_col=0)
 
     df["DATE"] = pd.to_datetime(df["DATE"])
     df = df.set_index("DATE").asfreq("B")
@@ -104,41 +156,6 @@ def load_data():
 
     return df
 
-
-def get_feature_desc(feature: str):
-    feature_clean = feature.replace("_YOY", "").replace("_QOQ", "").replace("_MOM", "").replace("_WOW", "")
-    feature_clean = re.sub(r"_\d{1,2}(?:YRS|MTHS|YR)", "", feature_clean)
-    desc = data_dictionary.loc[data_dictionary["FEATURE_NAME"] == feature_clean]
-    if desc.empty:
-        raise Exception(f"Feature {feature} not found in data dictionary")
-    return desc.iloc[0]["FEATURE_DESCRIPTION"]
-
-def print_features_not_in_data_dict(df, data_dict):
-    for feature in df.columns:
-        try:
-            get_feature_desc(feature)
-        except:
-            print(f"Feature {feature} not found in data dictionary")
-
-def plot_features(change):
-    feature = change['new']
-    try:
-        desc = get_feature_desc(feature)
-    except:
-        return
-    fig = px.line(df, x=df.index, y=feature, title=feature + " " + desc)
-    fig.update_layout(title={'font': {'size': 10}})
-    fig.show()
-
-
-def plot_forecast(real_values, forecast):
-    errors = forecast - real_values
-    fig = px.line()
-    fig.add_scatter(x=forecast.index, y=forecast, mode='lines', name='Forecast', line=dict(color='red'))
-    fig.add_scatter(x=real_values.index, y=real_values, mode='lines', name='Actual')
-    fig.add_bar(x=errors.index, y=errors, name='Errors')
-    fig.update_layout(title='Simple Exponential Smoothing vs Actual', xaxis_title='Date', yaxis_title='Value')
-    fig.show()
 
 def unscale_series(series: TimeSeries, pipeline: Pipeline, ts_scaled):
     series_start_time = series.start_time()
@@ -205,7 +222,6 @@ def get_ts_by_forecast_horizon(pred_df):
     return forecast_by_horizon
 
 
-
 def df2ts(df):
     # Create a TimeSeries object
     ts = TimeSeries.from_dataframe(df, value_cols=['US_TB_YIELD_10YRS']) #.add_holidays("US")
@@ -222,6 +238,9 @@ def df2ts(df):
     return ts, covars_diff, covars
 
 def scale_ts(series, should_diff):
+    """Scale TimeSeries and apply transformations"""
+
+
     log_transformer = InvertibleMapper(
         fn=np.log1p, inverse_fn=np.expm1, name="log1p"
     )
@@ -237,58 +256,6 @@ def scale_ts(series, should_diff):
         series_scaled = pipeline.fit_transform(series)[1:]
 
     return pipeline, series_scaled
-
-
-def plot_training_history(train_losses, val_losses):
-    # Create the figure
-    fig = go.Figure()
-
-    # Add traces for training and validation loss
-    fig.add_trace(
-        go.Scatter(
-            x=list(range(1, len(train_losses) + 1)),
-            y=train_losses,
-            name='Training Loss',
-            line=dict(color='blue')
-        )
-    )
-    
-    fig.add_trace(
-        go.Scatter(
-            x=list(range(1, len(val_losses) + 1)),
-            y=val_losses,
-            name='Validation Loss',
-            line=dict(color='red')
-        )
-    )
-
-    # Update layout
-    fig.update_layout(
-        title='Training and Validation Loss Over Time',
-        xaxis_title='Epoch',
-        yaxis_title='Loss',
-        hovermode='x unified',
-        template='plotly_white'
-    )
-
-    # Show the plot
-    fig.show()
-
-
-def evaluate_by_horizon(forecasts_df):
-    forecasts_df["abs_pct_error"] = ((forecasts_df["US_TB_YIELD_10YRS"] - forecasts_df["forecast"]) / forecasts_df["US_TB_YIELD_10YRS"]).abs()
-    forecasts_df["squared_error"] = forecasts_df["error"]**2
-    grouped = forecasts_df.groupby(by="horizon").mean()[["abs_pct_error", "squared_error"]].add_prefix("mean_")
-    grouped["root_mean_squared_error"] = grouped["mean_squared_error"]**0.5
-
-    return grouped
-
-
-def save_results(hparams, eval_metrics, output_path):
-    output_path = Path(output_path)
-    include_header = not output_path.exists()
-    results = pd.concat([pd.Series(hparams), eval_metrics.mean()])
-    pd.DataFrame(results).T.to_csv(output_path, mode="a", header=include_header, index=False)
 
 
 
